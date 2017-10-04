2e9fab48ebc42388ea08b503a873b1d6e2ae27e0
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2017-10-04 10:42:22 -0700

    The logo and text aligned and styled properly. Used Dimensions to center the logo

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, Image, Text } from 'react-native';
+import styles from './styles';
 
 const Logo = () => (
-    <View>
-        <Image source={require('./images/background.png')}>
-            <Image source={require('./images/logo.png')} />
+    <View style={styles.container}>
+        <Image resizeMode="contain" style={styles.imageContainer} source={require('./images/background.png')}>
+            <Image resizeMode="contain" style={styles.image} source={require('./images/logo.png')} />
         </Image>
-        <Text>Currency Converter</Text>
+        <Text style={styles.text} >Currency Converter</Text>
     </View>
 );
 
